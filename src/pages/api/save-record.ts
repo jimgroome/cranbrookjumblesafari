42ce599ab7eb71f254ffb396b8a6d0c3a29aeb93
Jim Groome <james@jgroome.com>
2023-04-22 07:52:34 +1000

    Save boolean as a string

--- src/pages/api/save-record.ts
+++ src/pages/api/save-record.ts
@@ -42,7 +42,7 @@ const saveRecord = async (req: NextApiRequest, res: NextApiResponse) => {
           LAT: lat ? lat.toString() : undefined,
           LONG: long ? long.toString() : undefined,
           RECORDTYPE: recordType,
-          AGREESIGN: agreeSign || undefined,
+          AGREESIGN: agreeSign ? agreeSign.toString() : undefined,
         },
       },
       {
