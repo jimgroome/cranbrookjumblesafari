27fa7a588a7265729381adfb9fe34f6d2a2d3c77
Jim Groome <james@jgroome.com>
2023-05-17 09:41:43 +0100

    Analytics

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,4 +1,5 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import { Html, Head, Main, NextScript } from "next/document";
+import Script from "next/script";
 
 export default function Document() {
   return (
@@ -7,7 +8,20 @@ export default function Document() {
       <body>
         <Main />
         <NextScript />
+        <Script
+          src={`https://www.googletagmanager.com/gtag/js?id=G-TGJBWFL89N`}
+          strategy="afterInteractive"
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+        window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+
+          gtag('config', 'G-TGJBWFL89N');
+        `}
+        </Script>
       </body>
     </Html>
-  )
+  );
 }
