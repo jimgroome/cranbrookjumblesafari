b303964ae7b60a8583ddd1913408fdee4df91cb1
Jim Groome <james@jgroome.com>
2023-06-05 19:29:16 +0100

    All finished for 2023

--- src/components/content.tsx
+++ src/components/content.tsx
@@ -23,21 +23,12 @@ const Content = () => {
           </Grid>
           <Grid item xs={12} mb={4}>
             <Typography variant="body1" align="center">
-              Sunday 4th June, 10am - 4pm
+              Thank you Cranbrook!
             </Typography>
           </Grid>
           <Grid item xs={12} mb={4}>
             <Typography variant="body1" mb={2}>
-              Woodpeckers Preschool is holding our first ever Cranbrook Jumble
-              Safari on Sunday 4th June 2023.
-            </Typography>
-            <Typography variant="body1" mb={2}>
-              Families all over Cranbrook are selling second hand bargains from
-              their garage or driveway.
-            </Typography>
-            <Typography variant="body1" mb={2}>
-              For just £1, collect a map with the locations of everyone taking
-              part from Woodpeckers Preschool, Quaker Lane, TN17 2HF.
+              Cranbrook Jumble Safari will return in Summer 2024.
             </Typography>
             {/* <Typography variant="body1" mb={2}>
               Order your map online now and we&rsquo;ll email it to you on the
@@ -48,30 +39,30 @@ const Content = () => {
           {/* <Grid item xs={12} mb={4}>
             <BuyMapModal />
           </Grid> */}
-          <Grid item xs={12} mb={4}>
-            <Typography variant="body1" mb={2}>
+          {/* <Grid item xs={12} mb={4}> */}
+          {/* <Typography variant="body1" mb={2}>
               Woodpeckers&rsquo; car park will be filled with stall holders
               selling toys, books, puzzles and more. Plus we&rsquo;ll be selling
               bacon rolls, cake, hot drinks and squash &mdash; so make sure you
               pay us a visit!
-            </Typography>
-            {/* <Typography variant="body1" mb={2} fontWeight={800}>
+            </Typography> */}
+          {/* <Typography variant="body1" mb={2} fontWeight={800}>
               Interested in taking part as a seller? It&rsquo;s not too late.
             </Typography>
             <Typography variant="body1" mb={2}>
               We&rsquo;re accepting entries until 8pm on Saturday 3rd June. It
               costs £10 to participate.
             </Typography> */}
-            <Typography variant="body1" mb={2}>
+          {/* <Typography variant="body1" mb={2}>
               All the proceeds from map sales go to Woodpeckers Preschool.
-            </Typography>
-          </Grid>
+            </Typography> */}
+          {/* </Grid> */}
           {/* <Grid item xs={12} mb={4}>
             <BookingModal />
           </Grid> */}
-          <Grid item xs={12} mb={2}>
+          {/* <Grid item xs={12} mb={2}>
             <FaqsModal />
-          </Grid>
+          </Grid> */}
         </Container>
       </div>
       <Footer />
