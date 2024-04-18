25409a9da5fc4abc4c89de70068a9fced021598d
Jim Groome <james@jgroome.com>
2024-04-18 20:50:17 +0100

    Hmm

--- src/pages/api/save-record.ts
+++ src/pages/api/save-record.ts
@@ -1,5 +1,4 @@
 import { NextApiRequest, NextApiResponse } from "next";
-import mailchimp from "@mailchimp/mailchimp_marketing";
 import { v4 as uuid } from "uuid";
 import AWS from "aws-sdk";
 
@@ -11,89 +10,25 @@ AWS.config.update({
 
 const saveRecord = async (req: NextApiRequest, res: NextApiResponse) => {
   try {
-    const { body } = req;
-
-    const record = { ...body, id: uuid() };
-
-    const {
-      recordType,
-      name,
-      email,
-      address1,
-      address2,
-      town,
-      postcode,
-      phone,
-      lat,
-      long,
-      agreeSign,
-      id,
-    } = record;
-
     const DynamoDB = new AWS.DynamoDB.DocumentClient();
     const now = new Date();
     const time = now.toISOString();
 
+    const { body } = req;
+
+    const record = { ...body, id: uuid(), time };
+
     await DynamoDB.put(
       {
         TableName: process.env.DYNAMODB_TABLE_NAME as string,
-        Item: {
-          recordType,
-          name,
-          email,
-          address1,
-          address2,
-          town,
-          postcode,
-          phone,
-          lat,
-          long,
-          agreeSign,
-          time,
-          id,
-        },
-        ConditionExpression: "attribute_not_exists(email)",
+        Item: record,
       },
       (error) => {
         if (error) {
-          if (error.statusCode === 400) {
-            return res.status(400).json({ response: "That didn't work" });
-          }
-        } else {
-          return res.status(200).json({
-            response: "Name added",
-          });
+          console.log(JSON.stringify(error, null, 4));
         }
       }
     );
-
-    // mailchimp.setConfig({
-    //   apiKey: process.env.MAILCHIMP_API_KEY,
-    //   server: "us14",
-    // });
-
-    // await mailchimp.lists.addListMember(
-    //   process.env.MAILCHIMP_lIST_ID as string,
-    //   {
-    //     email_address: email,
-    //     status: "transactional",
-    //     merge_fields: {
-    //       NAME: name,
-    //       PHONE: phone,
-    //       ADDRESS1: address1 || undefined,
-    //       ADDRESS2: address2 || undefined,
-    //       TOWN: town || undefined,
-    //       POSTCODE: postcode || undefined,
-    //       LAT: lat ? lat.toString() : undefined,
-    //       LONG: long ? long.toString() : undefined,
-    //       RECORDTYPE: recordType,
-    //       AGREESIGN: agreeSign ? agreeSign.toString() : undefined,
-    //     },
-    //   },
-    //   {
-    //     skipMergeValidation: false,
-    //   }
-    // );
 
     res.status(200).json({});
   } catch (error) {
